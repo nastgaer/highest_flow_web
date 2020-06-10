b366b74ad503d62282d73fc37806f24cebe67d88
goldstar081 <goldstar081@gmail.com>
2020-06-10 21:09:29 +0800

    .

---
+++ library/src/main/java/highest/flow/taobaolive/taobao/dao/TaobaoAccountDao.java
@@ -0,0 +1,10 @@
+package highest.flow.taobaolive.taobao.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import highest.flow.taobaolive.taobao.entity.TaobaoAccount;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface TaobaoAccountDao extends BaseMapper<TaobaoAccount> {
+
+}
