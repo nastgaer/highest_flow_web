1c294589bc25e88e7d05f12d8d31dc118ccf2579
goldstar081 <goldstar081@gmail.com>
2020-08-25 18:07:48 +0800

    .

--- xdata/src/main/java/highest/taolive/xdata/SignController.java
+++ xdata/src/main/java/highest/taolive/xdata/SignController.java
@@ -76,9 +76,69 @@ public class SignController {
 
             String jsonText = objectMapper.writeValueAsString(mTopSignParam);
 
-            logger.info("<< " + jsonText);
+//            logger.info("<< " + jsonText);
             String respText = minaService.sendMessage(jsonText);
-            logger.info(">> " + (respText == null ? "null" : respText));
+//            logger.info(">> " + (respText == null ? "null" : respText));
+
+            if (StringUtils.isNullOrEmpty(respText)) {
+                return R.error("空了");
+            }
+
+            JsonParser jsonParser = JsonParserFactory.getJsonParser();
+            Map<String, Object> map = jsonParser.parseMap(respText);
+            Map<String, Object> mapData = (Map) map.get("data");
+
+            String xsign = mapData == null || !mapData.containsKey("x-sign") ? "" : String.valueOf(mapData.get("x-sign"));
+            String miniWua = mapData == null || !mapData.containsKey("mini-wua") ? "" : String.valueOf(mapData.get("mini-wua"));
+
+            if (!StringUtils.isNullOrEmpty(xsign)) {
+                return R.ok("成功").put("xsign", xsign).put("encoded", false).put("mini-wua", miniWua);
+            }
+
+            return R.error("xsign验证失败");
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return R.error("xsign验证失败");
+    }
+
+    @PostMapping("/xdata2")
+    public R xsign2(@RequestParam(name = "data") String data, @RequestParam(name = "sign") String sign) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            XHeader xHeader = objectMapper.readValue(data, XHeader.class);
+
+            // Convert XHeader to MTopSignParam
+            MTopSignParam mTopSignParam = new MTopSignParam();
+
+            Map<String, String> p1 = new HashMap<>();
+            p1.put("utdid", xHeader.getUtdid());
+            p1.put("uid", xHeader.getUid());
+            p1.put("appKey", xHeader.getAppkey());
+            p1.put("pv", xHeader.getPv());
+            p1.put("t", String.valueOf(xHeader.getTimestamp()));
+            p1.put("sid", xHeader.getSid());
+            p1.put("ttid", xHeader.getTtid());
+            p1.put("deviceId", xHeader.getDevid());
+            p1.put("location", xHeader.getLocation1() + "," + xHeader.getLocation2());
+            p1.put("x-features", xHeader.getFeatures());
+            p1.put("v", xHeader.getUrlVer());
+            p1.put("api", xHeader.getSubUrl());
+            p1.put("data", xHeader.getData());
+
+            mTopSignParam.setP1(p1);
+            mTopSignParam.setP2("25443018");
+
+            if (!checkMTopSignParam(mTopSignParam)) {
+                return R.error(ErrorCodes.INVALID_PARAMETER, "不合法参数");
+            }
+
+            String jsonText = objectMapper.writeValueAsString(mTopSignParam);
+
+//            logger.info("<< " + jsonText);
+            String respText = minaService.sendMessage(jsonText);
+//            logger.info(">> " + (respText == null ? "null" : respText));
 
             if (StringUtils.isNullOrEmpty(respText)) {
                 return R.error("空了");
